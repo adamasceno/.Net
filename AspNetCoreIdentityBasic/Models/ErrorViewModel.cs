68a016cde2e7440c893712dfb75546086c55dbcc
Andre <adamasceno@navvia.com>
2018-05-28 18:13:58 -0400

    Add project files.

---
+++ AspNetCoreIdentityBasic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreIdentityBasic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
